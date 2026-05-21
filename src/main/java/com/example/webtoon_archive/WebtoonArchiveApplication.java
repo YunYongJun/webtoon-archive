da6b70385ba67bdb854410b4f2940d2da673db87
YunYongJun <a86622014@gmail.com>
2026-05-21 20:24:10 +0900

    feat: 초기 Spring Boot 프로젝트 생성

---
+++ src/main/java/com/example/webtoon_archive/WebtoonArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.webtoon_archive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtoonArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtoonArchiveApplication.class, args);
+	}
+
+}
