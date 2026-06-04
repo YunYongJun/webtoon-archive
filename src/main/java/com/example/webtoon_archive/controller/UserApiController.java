e5706624b2fe45ef818fbc7ff93bbf1cefcb0e91
YunYongJun <a86622014@gmail.com>
2026-06-05 00:12:42 +0900

    UserApiController 로그인 APi 추가

--- src/main/java/com/example/webtoon_archive/controller/UserApiController.java
+++ src/main/java/com/example/webtoon_archive/controller/UserApiController.java
@@ -1,6 +1,7 @@
 package com.example.webtoon_archive.controller;
 
 import com.example.webtoon_archive.dto.UserSignupRequest;
+import com.example.webtoon_archive.dto.UserLoginRequest;
 import com.example.webtoon_archive.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,5 +22,14 @@ public class UserApiController {
     public String signup(@RequestBody UserSignupRequest request) {
         userService.signup(request);
         return "회원가입이 성공적으로 완료되었습니다.";
+    }
+
+    /* 로그인 API 
+    * 주소: POST /api/users/login
+    * 성공 시 암호화된 JWT 토큰 문자열을 반환
+    */
+    @PostMapping("/login")
+    public String login(@RequestBody UserLoginRequest request) {
+        return userService.login(request);
     }
 }
